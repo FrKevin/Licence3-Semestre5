96b2bc55a7b3628ca53befaa1bd2249f9f1541c9
Kevin Gamelin <kevin-gamelin@hotmail.fr>
2015-10-14 21:28:08 +0200

    DPS -> TP4 -> test + graph

--- PDS/TP4/OUDJAIL GAMELIN/test.sh
+++ PDS/TP4/OUDJAIL GAMELIN/test.sh
@@ -1,43 +1,69 @@
 #! /bin/bash
-make;
 
-#Test tail sans paramètres
-echo "----------------------------------"
-echo "tail systeme normale (sans paramètre)";
-tail test/number.txt
+echo "--------- Creation des fichiers test --------"
+mkdir test
+touch test/text.txt
+touch test/vide
+
+echo "fhoshsdjvwjhvoiqhefbqbvjsiuhqzbejkfbohvbdjbsvoibfiozebufhn" >> test/text.txt
+echo "qhoihfsh<fpcoshcjhsdvobiwhdoisvhfshicjshvidvjbwuoufsfe<oshicdsw" >> test/text.txt
+echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" >> test/text.txt
+
+# création d'un gros fichier de 30 Mio environ
+echo "création d'un gros fichier de 130 Mio environ "
+touch test/big
+dd if=/dev/urandom of=test/big bs=131072 count=1000
+echo "--------------------- OK --------------------"
+
+echo -n "Définition de la variable globale pour les tests simples ... "
+MCAT_BUFSIZ=1024
+export MCAT_BUFSIZ
+echo "ok"
+
 echo ""
-echo "notre tail (sans paramètre)"
-./mtail test/number.txt
-echo "----------------------------------"
+
+
+#Test sur un fichier vide
+echo "---- Test: pour un fihcier vide ----"
+echo "cat systeme"
+cat test/vide
+echo "notre cat"
+./mcat test/vide
+echo "------------------------------------"
 
 echo ""
 
-#Test tail avec paramètres
-echo "----------------------------------"
-echo "tail systeme, argument -n 5";
-tail test/number.txt -n 5
-echo ""
-echo "notre du tail, argument -n 5"
-./mtail test/number.txt -n 5
-echo "----------------------------------"
+#Test sur un fichier avec texte
+echo "---- Test: pour un fihcier avec texte ----"
+echo "cat systeme"
+cat test/text.txt
+echo "notre cat"
+./mcat test/text.txt
+echo "------------------------------------"
 
 echo ""
 
-#Test tail 0 ligne
-echo "----------------------------------"
-echo "tail systeme -n 0";
-tail test/number.txt -n 0
-echo ""
-echo "notre du tail, argument -n 0"
-./mtail test/number.txt -n 0
-echo "----------------------------------"
+echo "------ Exécution des tests de performance, avec plusieurs tailles de buffer -------"
+#On supprime si il existe deja le fichier .dat
+rm -f time.dat && echo "#buffsize real user sys" > time.dat
 
+for size in `awk 'BEGIN { for( i=1; i<=8388608; i*=2 ) print i }'`;
+do
+    export MCAT_BUFSIZ=$size
+    echo -n "Test pour un buffer de $MCAT_BUFSIZ octets ... "
+    /usr/bin/time -f "$MCAT_BUFSIZ %e %U %S" ./mcat test/big > /dev/null 2>> time.dat
+    echo "ok"
+done
 
-#Test tail nombre de ligne < 0
-echo "----------------------------------"
-echo "tail systeme -m -10";
-tail test/number.txt -n -10
+echo -n "Génération du graph ... "
+gnuplot run.gnu
 echo ""
-echo "notre du tail, argument -n -10"
-./mtail test/number.txt -n -10
-echo "----------------------------------"
+echo "--------------------------------------- OK -----------------------------------------"
+
+echo ""
+
+echo "---------------------------"
+echo "Suppression du dossier test et du time.at"
+rm -r test
+rm time.dat
+echo "----------- OK ------------"
