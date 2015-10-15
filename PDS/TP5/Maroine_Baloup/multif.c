6d1191d6f7084870a2bd83f8969969526135f787
marcbal <marc.baloup@laposte.net>
2015-10-15 10:48:14 +0200

    Préparation du TP5

---
+++ PDS/TP5/Maroine_Baloup/multif.c
@@ -0,0 +1,40 @@
+/*
+ * Par Marc Baloup et Maxime Maroine 
+ * 
+ * Licence 3 Informatique - Université Lille 1
+ * 
+ * 2015-2016
+ */
+#include <unistd.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+typedef enum { false, true } bool;
+
+/* déclaration des fonctions */
+int multif(func_t functions[], int args[]);
+
+
+/* Main */
+int main(int argc, char** argv) {
+	
+	
+	return cat(argv[1], buffsize);
+}
+
+
+
+
+
+
+int multif(func_t functions[], int args[]) {
+	
+	return EXIT_SUCCESS;
+}
---
+++ PDS/TP5/Maroine_Baloup/readme.md
@@ -0,0 +1,12 @@
+TP4 : multif, race et exo8 (processus)
+=====
+
+
+Par Maxime Maroine et Marc Baloup
+
+Licence 3 Informatique, Université Lille 1
+
+2015-2016
+
+
+
