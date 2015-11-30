03c704781f67cfca22393157f6946a57545cfac4
zeroCrowsky <veis.oudjail@gmail.com>
2015-11-30 23:48:40 +0100

    PDS -> TP8 compteur_gc compile mais non verifier

--- PDS/TP8/OUDJAIL_GAMELIN/aleazard/src/compteur-gc.c
+++ PDS/TP8/OUDJAIL_GAMELIN/aleazard/src/compteur-gc.c
@@ -6,61 +6,78 @@
 #include <unistd.h>
 #include <time.h>
 #include <assert.h>
+#include <pthread.h>
 
-/* t_arg => thread_arg */
+/* Signature de fonction */
+unsigned long compteur_gc(char *bloc, unsigned long taille);
+void *wrapper(void *arg);
+long mtcompteur_gc(char* tampon, unsigned long taille, int nthread);
+
+/* tharg => thread_arg */
 typedef struct {
-  char* bloc;
+  char *bloc;
   unsigned long taille;
   unsigned long result;
-} t_arg;
-
-void* wrapper(void* arg){
-  t_arg* struct_compteur = (t_arg*) arg;
-  struct_compteur->result = compteur_gc(struct_compteur->bloc, struct_compteur->taille);
-  return NULL;
-}
-
-void compteur_gc_thrad(char* tampon, unsigned long taille, int nthread){
-  unsigned long i;
-  pthread_t* t_pthread;
-  unsigned long taille_segment;
-  unsigned long result = 0;
-
-  /* Exemple un buffer de 2 pour 4 thread */
-  if( taille < nthread){
-    return;
-  }
-
-  /* Creation du tablean de n thread */
-  t_pthread = (pthread_t*)malloc(sizeof(pthread_t)*nthread);
-  assert(t_pthread != NULL);
-
-  taille_segment = taille/nthread;
-  for(i=0 ; i<nthread-1; i++){
-    t_arg* struct_compteur_gc = (t_arg*)malloc(sizeof(t_arg));
-    assert(struct_compteur_gc != NULL);
-
-    struct_compteur_gc->result = 0;
-    struct_compteur_gc->bloc = tampon+i * nthread;
-    struct_compteur_gc->taille = taille_segment;
-    pthread_create(t_pthread[i], NULL, wrapper, struct_compteur_gc);
-  }
-  
-  pthread_create(t_pthread[nthread-1], NULL, wrapper, struct_compteur_gc);
-  for(i = 0; i < nthread; i++){
-    pthread_join(t_pthread[i], NULL);
-
-  }
-}
+} tharg_t;
 
 unsigned long compteur_gc(char *bloc, unsigned long taille) {
     unsigned long i, cptr = 0;
 
-    for (i = 0; i < taille; i++)
-        if (bloc[i] == 'G' || bloc[i] == 'C')
+    for (i = 0; i < taille; i++) {
+        if (bloc[i] == 'G' || bloc[i] == 'C') {
             cptr++;
-
+        }
+    }
     return cptr;
+}
+
+void *wrapper(void *arg) {
+  tharg_t *th_arg = (tharg_t*) arg;
+  th_arg->result = compteur_gc(th_arg->bloc, th_arg->taille);
+  return NULL;
+}
+
+long mtcompteur_gc(char* tampon, unsigned long taille, int nthread) {
+  unsigned long i;
+  pthread_t *t_pthread;
+  tharg_t **t_th_arg;
+  unsigned long segment_size;
+  unsigned long result = 0;
+
+  /* Allocation variables */
+  /* Tableau de threads */
+  t_pthread = (pthread_t*)malloc(sizeof(pthread_t)*nthread);
+  assert(t_pthread != NULL);
+  /* Tableau de pointeur d'argument de threads */
+  t_th_arg = (tharg_t **)malloc(sizeof(tharg_t *)*nthread);
+  assert(t_th_arg != NULL);
+
+  segment_size = taille/nthread;
+  for (i = 0 ; i < nthread-1; i++) {
+    t_th_arg[i] = (tharg_t *)malloc(sizeof(tharg_t));
+    assert(t_th_arg[i] != NULL);
+
+    t_th_arg[i]->result = 0;
+    t_th_arg[i]->bloc = tampon + i * segment_size;
+    t_th_arg[i]->taille = segment_size;
+    pthread_create(&t_pthread[i], NULL, wrapper, t_th_arg[i]);
+  }
+
+  t_th_arg[i]->result = 0; /* i = n-1 */
+  t_th_arg[i]->bloc = tampon + i * segment_size;
+  t_th_arg[i]->taille = taille - segment_size * i;
+  pthread_create(&t_pthread[i], NULL, wrapper, t_th_arg[i]);
+
+  /* Boucle d'attente */
+  for(i = 0; i < nthread; i++){
+    pthread_join(t_pthread[i], NULL);
+    result += t_th_arg[i]->result;
+    free(t_th_arg[i]);
+    t_th_arg[i] = NULL;
+  }
+  free(t_pthread);
+  t_pthread = NULL;
+  return result;
 }
 
 int main(int argc, char *argv[]) {
